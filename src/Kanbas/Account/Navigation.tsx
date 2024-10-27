0b80422b5860a7119bbf72032e56d2c02e7133ca
codehroll <judyh0665@gmail.com>
2024-10-26 22:44:03 -0700

    update a4 2.1-2.4.1

--- src/Kanbas/Account/Navigation.tsx
+++ src/Kanbas/Account/Navigation.tsx
@@ -1,26 +1,21 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export default function AccountNavigation() {
+  const links = ["Signin", "Signup", "Profile"];
+  const { pathname } = useLocation();
   return (
     <div id="wd-account-navigation" className="wd list-group fs-5 rounded-0">
-      <Link
-        to={`/Kanbas/Account/Signin`}
-        className="list-group-item active border border-0"
-      >
-        Signin
-      </Link>
-      <Link
-        to={`/Kanbas/Account/Signup`}
-        className="list-group-item text-danger border border-0"
-      >
-        Signup
-      </Link>
-      <Link
-        to={`/Kanbas/Account/Profile`}
-        className="list-group-item text-danger border border-0"
-      >
-        Profile
-      </Link>
+      {links.map((link) => (
+        <Link
+          key={link}
+          to={`/Kanbas/Account/${link}`}
+          className={`list-group-item border border-0 ${
+            pathname.includes(link) ? "text-black active" : "text-danger"
+          }`}
+        >
+          {link}
+        </Link>
+      ))}
     </div>
   );
 }
--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -1,26 +1,32 @@
 import Lab1 from "./Lab1";
 import Lab2 from "./Lab2";
 import Lab3 from "./Lab3";
+import Lab4 from "./Lab4";
 import TOC from "./TOC";
 import { Route, Routes, Navigate } from "react-router";
+import store from "./store";
+import { Provider } from "react-redux";
 
 export default function Labs() {
   return (
-    <div id="wd-labs" className="container-fluid">
-      <h4>
-        Di He, Section 1{" "}
-        <a href="https://github.com/codehroll/kanbas-react-web-app/tree/a2">
-          Source Code
-        </a>
-      </h4>
-      <h1>Labs</h1>
-      <TOC />
-      <Routes>
-        <Route path="/" element={<Navigate to="Lab1" />} />
-        <Route path="Lab1" element={<Lab1 />} />
-        <Route path="Lab2" element={<Lab2 />} />
-        <Route path="Lab3/*" element={<Lab3 />} />
-      </Routes>
-    </div>
+    <Provider store={store}>
+      <div id="wd-labs" className="container-fluid">
+        <h4>
+          Di He, Section 1{" "}
+          <a href="https://github.com/codehroll/kanbas-react-web-app/tree/a2">
+            Source Code
+          </a>
+        </h4>
+        <h1>Labs</h1>
+        <TOC />
+        <Routes>
+          <Route path="/" element={<Navigate to="Lab1" />} />
+          <Route path="Lab1" element={<Lab1 />} />
+          <Route path="Lab2" element={<Lab2 />} />
+          <Route path="Lab3/*" element={<Lab3 />} />
+          <Route path="Lab4" element={<Lab4 />} />
+        </Routes>
+      </div>
+    </Provider>
   );
 }
