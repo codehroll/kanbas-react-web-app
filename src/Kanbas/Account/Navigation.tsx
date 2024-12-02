20500a43202e8e53563d9ec3da935cc7dcffd0f8
codehroll <judyh0665@gmail.com>
2024-12-01 22:19:20 -0800

    update a6 1-3.6.5

--- src/Kanbas/Account/Navigation.tsx
+++ src/Kanbas/Account/Navigation.tsx
@@ -4,6 +4,7 @@ import { useSelector } from "react-redux";
 export default function AccountNavigation() {
   const { currentUser } = useSelector((state: any) => state.accountReducer);
   const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
+  const active = (path: string) => (pathname.includes(path) ? "active" : "");
   const { pathname } = useLocation();
   return (
     <div id="wd-account-navigation" className="wd list-group fs-5 rounded-0">
@@ -12,12 +13,22 @@ export default function AccountNavigation() {
           key={link}
           to={`/Kanbas/Account/${link}`}
           className={`list-group-item border border-0 ${
-            pathname.includes(link) ? "text-black active" : "text-danger"
+            active(link) ? "text-black active" : "text-danger"
           }`}
         >
           {link}
         </Link>
       ))}
+      {currentUser && currentUser.role === "ADMIN" && (
+        <Link
+          to={`/Kanbas/Account/Users`}
+          className={`list-group-item border border-0 ${
+            active("Users") ? "text-black active" : "text-danger"
+          }`}
+        >
+          Users
+        </Link>
+      )}
     </div>
   );
 }
